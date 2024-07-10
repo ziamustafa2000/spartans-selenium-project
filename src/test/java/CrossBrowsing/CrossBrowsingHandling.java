cab6a38b1a1bc8e9ded78ed09653953544b33130
zianzahid <ziamustafa2000@hotmail.com>
2024-07-10 16:55:38 -0700

    spartans-selenium-project Cross Browsing Handling

--- src/test/java/CrossBrowsing/CrossBrowsingHandling.java
+++ src/test/java/CrossBrowsing/CrossBrowsingHandling.java
@@ -6,19 +6,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Scanner;
+
 public class CrossBrowsingHandling {
 
         public static void main(String[] args) {
-
-            String browserType = "edge";
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Choose browser chrome, firefox and edge");
+            scn.next();
+            String browserType = scn.next();
 
             WebDriver driver;
 
-            if (browserType.equals("chrome")) {
+            if (browserType.equalsIgnoreCase("chrome")) {
                 driver = new ChromeDriver();
-            } else if (browserType.equals("firefox")) {
+            } else if (browserType.equalsIgnoreCase("firefox")) {
                 driver = new FirefoxDriver();
-            } else if (browserType.equals("edge")) {
+            } else if (browserType.equalsIgnoreCase("edge")) {
                 driver = new EdgeDriver();
             }else {
                 throw new RuntimeException("Wrong driver");
