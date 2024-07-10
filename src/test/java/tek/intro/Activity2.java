e9f3892bb7017e0b7f1dad6702a69e15054b30e5
zianzahid <ziamustafa2000@hotmail.com>
2024-07-09 18:46:18 -0700

    spartans-selenium-project Activity2

--- src/test/java/tek/intro/Activity2.java
+++ src/test/java/tek/intro/Activity2.java
@@ -2,16 +2,15 @@ package tek.intro;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Activity2 {
 
     public static void main(String[] args) {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://dev.insurance.tekschool-students.com/");
-
-        String title = chromeDriver.getTitle();
-        System.out.println(title);
-
-        chromeDriver.close();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://dev.insurance.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.getTitle();
+        driver.quit();
     }
 }
 
