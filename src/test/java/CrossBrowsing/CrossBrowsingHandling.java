5aa23fc969b82512395dec335ca3e2b98b39d6c0
zianzahid <ziamustafa2000@hotmail.com>
2024-07-10 16:45:07 -0700

    spartans-selenium-project Cross Browsing Handling

---
+++ src/test/java/CrossBrowsing/CrossBrowsingHandling.java
@@ -0,0 +1,29 @@
+package CrossBrowsing;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CrossBrowsingHandling {
+
+        public static void main(String[] args) {
+
+            String browserType = "edge";
+
+            WebDriver driver;
+
+            if (browserType.equals("chrome")) {
+                driver = new ChromeDriver();
+            } else if (browserType.equals("firefox")) {
+                driver = new FirefoxDriver();
+            } else if (browserType.equals("edge")) {
+                driver = new EdgeDriver();
+            }else {
+                throw new RuntimeException("Wrong driver");
+            }
+
+            driver.get("https://google.com");
+        }
+    }
