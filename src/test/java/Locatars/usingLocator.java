2a6918015947821576356e39b72f9d8cd87786ca
zianzahid <ziamustafa2000@hotmail.com>
2024-07-10 18:29:21 -0700

    spartans-selenium-project Cross Browsing Handling

---
+++ src/test/java/Locatars/usingLocator.java
@@ -0,0 +1,32 @@
+package Locatars;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class usingLocator {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.manage().window().maximize();
+        driver.get("https://retail.tekschool-students.com/");
+
+        // locate the Element
+        By searchProductName = By.id("productName");
+        //find the Element
+        WebElement searchInputElement = driver.findElement(searchProductName);
+        // Preform Action
+        searchInputElement.sendKeys("plug");
+
+        By searchButton = By.id("searchButton");
+        WebElement searchButtonElement = driver.findElement(searchButton);
+        searchButtonElement.click();
+
+    }
+}
