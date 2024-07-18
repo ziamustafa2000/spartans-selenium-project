564861325d634dc308242c020d979e4974c5bdd5
zianzahid <ziamustafa2000@hotmail.com>
2024-07-17 17:56:37 -0700

    spartans-selenium-project July16

---
+++ src/test/java/Locatars/CSS/Css_activity.java
@@ -0,0 +1,35 @@
+package Locatars.CSS;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.awt.SystemColor.text;
+
+public class Css_activity {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.cssSelector("#signinLink")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("john2000@gmail.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("786@Allah");
+        driver.findElement(By.cssSelector("#loginBtn")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#accountLink")).click();
+        driver.findElement(By.cssSelector(".account__information-email")).getText();
+        System.out.println(text);
+        //driver.findElement(By.cssSelector("a#accountLink")).click();
+        String email = driver.findElement(By.cssSelector(".account__information-email")).getText();
+        System.out.println("Email: " + email);
+        driver.quit();
+        //login__subtitle #loginBtn #accountLink //.account__information-email
+    }
+
+}
